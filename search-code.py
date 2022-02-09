81f5ac3b6bc426c8e651c6916f168a2ef1e6d6ad
Teresa Slaven-Blair <44489920+EnSlavingBlair@users.noreply.github.com>
2022-02-09 13:29:27 +0800

    Tidy up code
    Made a few changes to streamline code and make it fit better with coding styles

--- search-code.py
+++ search-code.py
@@ -3,35 +3,12 @@ from collections import Counter
 import matplotlib.pyplot as plt
 import operator
 
-words = np.zeros(2315)
-
-with open("/home/tess/Documents/Wordle/eligible-words.txt", "r") as f:
-	words = f.read().splitlines()
-
-letters = ''
-for word in words:
-	letters += word
-
-print(words)
-print(letters)
-
-letters_dict = Counter(letters)
-
-print(letters_dict)
-
-letters_list = letters_dict.items()
-
-def sort_alph(some_list):
+def sort_alph(some_dict):
 	# Sort alphabetically
+	some_list = some_dict.items()
 	sorted_alph = sorted(some_list)
 	alph_x, alph_y = zip(*sorted_alph)
 	return alph_x, alph_y
-
-letter_alph, value_alph = sort_alph(letters_list)
-plt.bar(letter_alph, value_alph)
-plt.title("Frequency of letters in possible Wordle answers\nAlphabetically sorted")
-plt.savefig("/home/tess/Documents/Wordle/letter-freq-alph.png")
-plt.clf()
 
 def sort_val(some_dict):
 	# Sort numerically
@@ -39,13 +16,36 @@ def sort_val(some_dict):
 	val_x, val_y = zip(*sorted_val)
 	return val_x, val_y
 
+### Read words from file
+with open("/home/tess/Documents/Wordle/eligible-words.txt", "r") as f:
+	words = f.read().splitlines()
+
+### Contatinate words into one string
+letters = ''
+for word in words:
+	letters += word
+
+### Get frequency of letters
+letters_dict = Counter(letters)
+
+### Sort and separate letters and values
+letter_alph, value_alph = sort_alph(letters_dict)
 letter_val, value_val = sort_val(letters_dict)
+
+### Plot frequency of letters
+
+# Alphabetically
+plt.bar(letter_alph, value_alph)
+plt.title("Frequency of letters in possible Wordle answers\nAlphabetically sorted")
+plt.savefig("/home/tess/Documents/Wordle/letter-freq-alph.png")
+plt.clf()
+
+# Numerically
 plt.bar(letter_val, value_val)
 plt.title("Frequency of letters in possible Wordle answers\nNumerically sorted")
 plt.savefig("/home/tess/Documents/Wordle/letter-freq-val.png")
 plt.clf()
 
 ### TO DO: Make list of letter frequencies given one or many letters are either present o not present
-
 
 
