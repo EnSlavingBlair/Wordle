608f90cf809b39d83cd212ac7d46c861d311a347
Teresa Slaven-Blair <44489920+EnSlavingBlair@users.noreply.github.com>
2022-02-13 07:48:57 +0800

    Letter location functionality
    Added the ability to tell it where letters can and can't be

--- search-code.py
+++ search-code.py
@@ -32,27 +32,35 @@ def search_letters(in_words, letter, operation):
 		pass
 	return out_words
 
-def plot_word_frequency(in_words, in_string):
+def plot_word_frequency(in_words, num, in_string):
 	### get letter frequencies and plot them
-	# Concatinate words into one string
+	## Concatinate words into one string
 	letters = ''
 	for word in in_words:
 		letters += word
-	# Get frequency of letters
+	## Get frequency of letters
 	letters_dict = Counter(letters)
-	# Sort and separate letters and values
-	letter_alph, value_alph = sort_alph(letters_dict)
+	## Sort and separate letters and values
+	# letter_alph, value_alph = sort_alph(letters_dict)
+	# perc_alph = np.around(np.array(value_alph)/num*100,0)
 	letter_val, value_val = sort_val(letters_dict)
-	# Plot frequency of letters
-	# Alphabetically
-	plt.bar(letter_alph, value_alph)
-	plt.title("Frequency of letters in possible Wordle answers\nAlphabetically sorted")
-	plt.savefig("/home/tess/Documents/Wordle/letter_freq_"+in_string+"_alph.png")
-	plt.clf()
-	# Numerically
-	plt.bar(letter_val, value_val)
+	perc_val = np.around(np.array(value_val)/num*100,0)
+	## Plot frequency of letters
+	## Alphabetically
+	# plt.bar(letter_alph, perc_alph)
+	# plt.title("Frequency of letters in possible Wordle answers\nAlphabetically sorted")
+	# #plt.savefig("/home/tess/Documents/Wordle/Wordle-main/letter_freq_"+in_string+"_alph.png")
+	# plt.ylabel("Percentage (%)")
+	# plt.xlabel("Letters")
+	# plt.show()
+	# plt.clf()
+	## Numerically
+	plt.bar(letter_val, perc_val)
 	plt.title("Frequency of letters in possible Wordle answers\nNumerically sorted")
-	plt.savefig("/home/tess/Documents/Wordle/letter_freq_"+in_string+"_val.png")
+	#plt.savefig("/home/tess/Documents/Wordle/Wordle-main/letter_freq_"+in_string+"_val.png")
+	plt.ylabel("Percentage of words containing each letter (%)")
+	plt.xlabel("Letters")
+	plt.show()
 	plt.clf()
 
 def pop_known(in_words, in_letters):
@@ -63,17 +71,47 @@ def pop_known(in_words, in_letters):
 	out_letters = "".join(c for c in letters if c.lower() not in in_letters)
 	return out_letters
 
+def get_good_locations(locations, in_words):
+	# deal with known correct letter locations
+	loc_num = 0
+	temp_words = []
+	for loc in locations:
+		if loc != '':
+			for word in in_words:
+				# search for letter in location index loc_num
+				if word[loc_num] == loc:
+					temp_words.append(word)
+		loc_num += 1
+	return temp_words
+
+def get_bad_locations(locations, in_words):
+	# deal with known incorrect letter locations
+	loc_num = 0
+	for loc in locations:
+		if loc != '':
+			for elem in loc: # to deal with multiple letters
+				for word in in_words:
+					# search for letter in location index loc_num
+					if word[loc_num] == elem:
+						in_words.remove(word)
+		loc_num += 1
+	return in_words
+
 ### Read words from file
-with open("/home/tess/Documents/Wordle/eligible-words.txt", "r") as f:
+with open("/home/tess/Documents/Wordle/Wordle-main/eligible-words.txt", "r") as f:
 	words = f.read().splitlines()
 
-plot_word_frequency(words, "+00")
+# plot_word_frequency(words, len(words), "+00")
 
 ### After some guesses
 
+#################### USER INPUT ####################
 # known letters both included or not included
-include_letters = "uor"
-eliminate_letters = "adiesnt"
+include_letters = "eu"
+eliminate_letters = "adi"
+good_locations = ['','','','e','']
+bad_locations = ['','','','','u']
+####################################################
 
 # get words that include known letters
 new_words = words
@@ -90,13 +128,15 @@ if eliminate_letters != "":
 unknown_letters = pop_known(new_words, include_letters)
 
 # plot stats
-plot_word_frequency(unknown_letters, "+"+include_letters+"-"+eliminate_letters)
+# plot_word_frequency(unknown_letters, len(new_words), "+"+include_letters+"-"+eliminate_letters)
+
+new_words = get_good_locations(good_locations, new_words)
+new_words = get_bad_locations(bad_locations, new_words)
 
 # print to user
 print("Possible words: ")
 print(new_words)
 
-### TO DO: Add functionality that takes into account known position of letters
+# plot_word_frequency(new_words, len(new_words), "locations")
 
-
-
+### TO DO: Plot expectation values and sort remaining possible words based on that
